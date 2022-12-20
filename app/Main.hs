4e5a84d4c542fcf2de00c86dacad21c7878a6154
DavidMazarro <davidmazarro98@gmail.com>
2022-12-21 00:28:55 +0100

    Added formatting with Fourmolu

--- app/Main.hs
+++ app/Main.hs
@@ -1,67 +1,72 @@
-module Main
-  ( main
-  ) where
+module Main (
+  main,
+) where
 
-import           Control.Exception              ( ErrorCall(ErrorCall)
-                                                , throwIO
-                                                )
-import           System.Console.ANSI
+import Control.Exception (
+  ErrorCall (ErrorCall),
+  throwIO,
+ )
+import System.Console.ANSI
 
+import Control.Monad (when)
+import Data.Version (showVersion)
+import GHC.IO.Exception (ExitCode (ExitSuccess))
 
-import           Control.Monad                  ( when )
-import           Data.Version                   ( showVersion )
-import           GHC.IO.Exception               ( ExitCode(ExitSuccess) )
 -- import           GHC.IO.Exception
-import           Options.Applicative
-import           Options.Applicative.Extra      ( helperWith )
-import           Path
-import           Paths_wasm_verify              ( version )
-import           System.Process                 ( readProcessWithExitCode )
-import           Text.Pretty.Simple             ( pPrint )
-import           WasmVerify.ModuleLoader        ( loadModuleFromFile )
+import Options.Applicative
+import Options.Applicative.Extra (helperWith)
+import Path
+import Paths_wasm_verify (version)
+import System.Process (readProcessWithExitCode)
+import Text.Pretty.Simple (pPrint)
+import WasmVerify.ModuleLoader (loadModuleFromFile)
 
 ----------------------------------------------------------------------------
 
 data Options = Options
-  { file         :: FilePath
-  , debugWasmADT :: Bool
+  { file :: FilePath,
+    debugWasmADT :: Bool
   }
 
 parserInfo :: ParserInfo Options
-parserInfo = info
-  (helpParser <*> versionParser <*> optsParser)
-  (  fullDesc
-  <> progDesc "A proof-of-concept formal verification tool for WASM"
-  <> header
-       "wasm-verify - A proof-of-concept formal verification tool for WASM"
-  )
+parserInfo =
+  info
+    (helpParser <*> versionParser <*> optsParser)
+    ( fullDesc
+        <> progDesc "A proof-of-concept formal verification tool for WASM"
+        <> header
+          "wasm-verify - A proof-of-concept formal verification tool for WASM"
+    )
  where
   helpParser :: Parser (a -> a)
-  helpParser = helperWith
-    (long "help" <> short 'h' <> help "Displays help for the different commands"
-    )
+  helpParser =
+    helperWith
+      ( long "help" <> short 'h' <> help "Displays help for the different commands"
+      )
   versionParser :: Parser (a -> a)
-  versionParser = infoOption
-    ("wasm-verify: " <> showVersion version)
-    (long "version" <> short 'v' <> help "Show version information")
+  versionParser =
+    infoOption
+      ("wasm-verify: " <> showVersion version)
+      (long "version" <> short 'v' <> help "Show version information")
   optsParser :: Parser Options
   optsParser =
     Options
       <$> strOption
-            (long "filepath" <> short 'f' <> help "WASM binary file to print")
+        (long "filepath" <> short 'f' <> help "WASM binary file to print")
       <*> switch
-            (long "debug-wasm-adt" <> help
+        ( long "debug-wasm-adt"
+            <> help
               "Outputs the Haskell ADT representation of the WASM module"
-            )
+        )
 
 ----------------------------------------------------------------------------
 -- Main
 
 main :: IO ()
 main = do
-  Options {..} <- execParser parserInfo
-  filepath     <- parseAbsFile file
-  fileExt      <- fileExtension filepath
+  Options{..} <- execParser parserInfo
+  filepath <- parseAbsFile file
+  fileExt <- fileExtension filepath
   case fileExt of
     ".smt2" -> do
       runZ3WithFile filepath
@@ -71,14 +76,16 @@ main = do
     _ -> do
       fail "The file extension must be .smt2 or .wasm"
 
--- | Runs the Z3 solver with the contents of the provided SMTLIB2 file
--- and outputs the results in the console.
+{- | Runs the Z3 solver with the contents of the provided SMTLIB2 file
+ and outputs the results in the console.
+-}
 runZ3WithFile :: Path Abs File -> IO ()
 runZ3WithFile filepath = do
-  (exitCode, stdout, stderr) <- readProcessWithExitCode
-    "z3"
-    ["-smt2", fromAbsFile filepath]
-    ""
+  (exitCode, stdout, stderr) <-
+    readProcessWithExitCode
+      "z3"
+      ["-smt2", fromAbsFile filepath]
+      ""
 
   putStrLn stdout
 
--- src/WasmVerify/ModuleLoader.hs
+++ src/WasmVerify/ModuleLoader.hs
@@ -1,25 +1,27 @@
 -- | Provides WASM module loading functions.
-module WasmVerify.ModuleLoader
-  ( loadModuleFromFile
-  ) where
+module WasmVerify.ModuleLoader (
+  loadModuleFromFile,
+) where
 
-import           Control.Exception.Safe         ( throwString )
-import qualified Data.ByteString               as BS
-                                                ( readFile )
-import qualified Language.Wasm                 as Wasm
-import           Path
+import Control.Exception.Safe (throwString)
+import qualified Data.ByteString as BS (
+  readFile,
+ )
+import qualified Language.Wasm as Wasm
+import Path
 
 ----------------------------------------------------------------------------
 
--- | Loads a binary WASM module provided a file path. 
--- In case there's an error while reading the file,
--- or a 'Wasm.ValidationError' comes up when validating the WASM module,
--- a t'Control.Exception.Safe.StringException' is thrown.
+{- | Loads a binary WASM module provided a file path.
+ In case there's an error while reading the file,
+ or a 'Wasm.ValidationError' comes up when validating the WASM module,
+ a t'Control.Exception.Safe.StringException' is thrown.
+-}
 loadModuleFromFile :: Path Abs File -> IO Wasm.ValidModule
 loadModuleFromFile filepath = do
   fileContents <- BS.readFile $ fromAbsFile filepath
   case Wasm.decode fileContents of
-    Left  str  -> throwString str
+    Left str -> throwString str
     Right wasm -> case Wasm.validate wasm of
-      Left  validationErr -> throwString $ show validationErr
-      Right validWasm     -> return validWasm
+      Left validationErr -> throwString $ show validationErr
+      Right validWasm -> return validWasm
