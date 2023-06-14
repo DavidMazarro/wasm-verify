0fbf453154f1bdbbcf0072e4fd0d1c48b898892e
DavidMazarro <davidmazarro98@gmail.com>
2023-06-14 22:43:16 +0200

    refactor(app): Removed now unused `runZ3WithFile` function

--- app/Main.hs
+++ app/Main.hs
@@ -2,17 +2,8 @@ module Main (
   main,
 ) where
 
-import Control.Exception (
-  ErrorCall (ErrorCall),
-  throwIO,
- )
 import Control.Monad (void, when)
 import Data.Version (showVersion)
-import GHC.IO.Exception (ExitCode (ExitSuccess))
-
--- import           GHC.IO.Exception
-import qualified Data.Text.IO as T
-import Helpers.ANSI (colorInRed)
 import qualified Language.Wasm as Wasm
 import qualified Language.Wasm.Structure as Wasm
 import Options.Applicative
@@ -20,7 +11,6 @@ import Options.Applicative.Extra (helperWith)
 import Path
 import Paths_wasm_verify (version)
 import System.Directory (makeAbsolute)
-import System.Process (readProcessWithExitCode)
 import Text.Pretty.Simple (pPrint)
 import VerifiWASM.Parser (parseVerifiWASMFile)
 import VerifiWASM.Validation (validate)
@@ -123,25 +113,3 @@ main = do
           fail "The specification file extension must be .verifiwasm"
     _ -> do
       fail "The WebAssembly module file extension must be .wasm"
-
-{- | Runs the Z3 solver with the contents of the provided SMTLIB2 file
- and outputs the results in the console.
--}
-runZ3WithFile :: Path Abs File -> IO ()
-runZ3WithFile filepath = do
-  (exitCode, stdout, stderr) <-
-    readProcessWithExitCode
-      "z3"
-      ["-smt2", fromAbsFile filepath]
-      ""
-
-  when (exitCode /= ExitSuccess) $ T.putStrLn . colorInRed $ "Z3 finished with errors:\n"
-  putStrLn stdout
-
-  -- Throw a Haskell error if the exit code is non-zero
-  -- and log the stderr of Z3
-  when (exitCode /= ExitSuccess) $ do
-    T.putStrLn . colorInRed $ "Z3 finished with the following errors (obtained from stderr):"
-    putStrLn stderr
-    T.putStrLn "──────────────────────────────────────"
-    throwIO $ ErrorCall "command failed"
